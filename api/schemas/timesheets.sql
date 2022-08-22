a9728687b7cf25493d301a15d89bd51a9bd31fbd
Gareth Lee <lkl.g@hotmail.com>
2022-08-23 00:37:28 +1000

    timesheets_seed.sql initial.

--- api/schemas/timesheets.sql
+++ api/schemas/timesheets.sql
@@ -2,10 +2,11 @@ DROP TABLE IF EXISTS timesheets CASCADE;
 
 CREATE TABLE timesheets (
     id SERIAL PRIMARY KEY,
-    name TEXT NOT NULL,
     user_id SERIAL REFERENCES users (id) ON DELETE CASCADE,
     client_id SERIAL REFERENCES clients (id) ON DELETE CASCADE,
-    date_start DATE,
-    date_end DATE,
-    total_duration INTEGER
+    project_id SERIAL REFERENCES projects (id) ON DELETE CASCADE,
+    activity_id SERIAL REFERENCES activities (id) ON DELETE CASCADE,
+    date DATE NOT NULL,
+    duration INTEGER NOT NULL,
+    comments TEXT,
 )
