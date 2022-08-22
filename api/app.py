6b6990f4fde24dec0ac1a27d18c8cce9b3ce95c1
Gareth Lee <lkl.g@hotmail.com>
2022-08-23 01:07:55 +1000

    DB pre-seed.

--- api/app.py
+++ api/app.py
@@ -12,3 +12,16 @@ import os
 import cloudinary.uploader
 
 from .db import get_db, close_db
+
+app = Flask(__name__)
+app.secret_key = os.environ.get('SECRET_KEY')
+
+@app.before_request
+def connect_to_db():
+    get_db()
+
+@app.after_request
+def disconnect_from_db(response):
+    close_db()
+    return response
+
