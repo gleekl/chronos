bfc412967034216986ce9c986a2fcd70b8558d4d
Gareth Lee <lkl.g@hotmail.com>
2022-08-22 18:50:20 +1000

    .gitignore updated. db.py updated with get_db() and close_db().

--- api/db.py
+++ api/db.py
@@ -1,3 +1,4 @@
+from numbers import Real
 from flask import g
 import psycopg2
 from psycopg2.extras import RealDictCursor
@@ -5,6 +6,21 @@ import os
 
 def get_db():
     if 'db' not in g:
-        connection = psycopg2.connecti(
-            host = os.environ.get('DB_HOST')
+        connection = psycopg2.connect(
+            host = os.environ.get('DB_HOST'),
+            database = os.environ.get('DB_DATABASE'),
+            user = os.environ.get('DB_USER'),
+            password = os.environ.get('DB_PASSWORD')
         )
+        cursor = connection.cursor(cursor_factory=RealDictCursor)
+        g.db = {
+            'connection': connection,
+            'cursor': cursor
+        }
+    return g.db
+
+def close_db():
+    db = g.pop('db', None)
+    if db is not None:
+        db['cursor'].close()
+        db['connection'].close()
