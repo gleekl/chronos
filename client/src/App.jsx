5af8c8287f2d1d94cd5512bef3a1d90b5792b203
Gareth Lee <lkl.g@hotmail.com>
2022-08-23 17:04:12 +1000

    NavigationBar set up.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,16 @@
 import { useState, useEffect } from 'react';
+import { Routes, Route, useNavigate } from "react-router-dom";
+
 import './App.css';
+import NavigationBar from './components/Navigation/NavigationBar';
 
 const App = () => {
   return (
     <div className="App">
-      <h1>Test</h1>
+      <NavigationBar />
+      <main>
+        <h1>Test</h1>
+      </main>
     </div>
   );
 }
---
+++ client/src/components/Navigation/NavigationBar.jsx
@@ -0,0 +1,97 @@
+import { NavLink } from "react-router-dom";
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Drawer from '@mui/material/Drawer';
+import CssBaseline from '@mui/material/CssBaseline';
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import List from '@mui/material/List';
+import Typography from '@mui/material/Typography';
+import Divider from '@mui/material/Divider';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import InboxIcon from '@mui/icons-material/MoveToInbox';
+import MailIcon from '@mui/icons-material/Mail';
+import DashboardIcon from '@mui/icons-material/Dashboard';
+import ProfileIcon from '@mui/icons-material/Person';
+import ProjectIcon from '@mui/icons-material/HomeRepairService';
+import TimeIcon from '@mui/icons-material/AccessTime';
+import ChartIcon from '@mui/icons-material/InsertChart';
+import ArrowIcon from '@mui/icons-material/ArrowForwardIos';
+
+const drawerWidth = 240;
+
+const navItems = [
+    {
+        text: "Dashboard",
+        component: <DashboardIcon />,
+        link: "/"
+    },
+    {
+        text: "Profile",
+        component: <ProfileIcon />,
+        link: "/profile"
+    },
+    {
+        text: "Projects",
+        component: <ProjectIcon />,
+        link: "/projects"
+    },
+    {
+        text: "Timesheets",
+        component: <TimeIcon />,
+        link: "/timsheets"
+    },
+    {
+        text: "Reports",
+        component: <ChartIcon />,
+        link: "/reports"
+    }
+]
+
+const NavigationBar = () => {
+    return (
+        <Box sx={{ display: 'flex' }}>
+            <CssBaseline />
+            <Drawer
+                sx={{
+                    width: drawerWidth,
+                    flexShrink: 0,
+                    '& .MuiDrawer-paper': {
+                        width: drawerWidth,
+                        boxSizing: 'border-box',
+                    },
+                }}
+                variant="permanent"
+                anchor="left"
+            >
+                <Toolbar />
+                <Divider />
+                <List>
+                    {navItems.map(({ text, component, link }) => (
+                        <NavLink to={link} className="navlink">
+                            <ListItem key={text} disablePadding>
+                                <ListItemButton>
+                                    <ListItemIcon>
+                                        {component}
+                                    </ListItemIcon>
+                                    <ListItemText primary={text} />
+                                </ListItemButton>
+                            </ListItem>
+                        </NavLink>
+                    ))}
+                </List>
+                <Divider />
+            </Drawer>
+            <Box
+                component="main"
+                sx={{ flexGrow: 1, bgcolor: 'background.default', p: 3 }}
+            >
+            </Box>
+        </Box>
+    );
+}
+
+export default NavigationBar
